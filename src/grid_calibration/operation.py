c9fde5c50265e6b3af17539ddd1285020778dfab
Yiqi Liu <liuyiqi20@mails.tsinghua.edu.cn>
2023-08-25 22:51:08 +0800

    feature: add ec_operation and json files for 07 (#6)

--- src/grid_calibration/operation.py
+++ src/grid_calibration/operation.py
@@ -272,6 +272,59 @@ class TB_operation_07B(TB_operation_05B):
         fit_config = file_lib.Fit_config(self.fit_range[basename])
         return [read_config, bkg_read_config, spectrum_config, fit_config]
     
+class EC_operation_07B(EC_operation_05B):
+    def __init__(self, tb_result_path:str, fit_range:str, energy:str, bkg_form:str, x_path, src_path, save_path, save_fig_path, result_path) -> None:
+        # read config
+        self.x_config = ""
+
+        # spectrum config
+        ref_temp = 25
+        ref_bias = 28.5 
+        tb_result:List[Dict[str, Any]] = util.json_load(tb_result_path)
+        ref_func = [lambda t,b: util.tempbias2DFunction(t, b, c['G0'],c['k'],c['V0'],c['b'],c['c']) for c in tb_result]
+        self.corr = [lambda t,b: f(ref_temp, ref_bias)/f(t,b) for f in ref_func]
+        self.adc_max = 65535.0
+        self.bin_width = 10
+        # fit config
+        self.fit_range = util.json_load(fit_range)
+        self.bkg_form = util.json_load(bkg_form)
+        # ec file process
+        self.x_path = x_path
+        self.src_path = src_path
+        self.save_path = save_path
+        self.save_fig_path = save_fig_path
+        self.result_path = result_path
+        self.x_ch = [f for f in os.listdir(self.x_path) if '_ch' in f]
+        
+        self.x_list = list(set([f.split('_')[2] for f in self.x_ch]))
+        self.x_list.sort()
+        # self.src_list = [f for f in os.listdir(self.src_path) if 'rundata' in f and 'bkg' not in f and 'CI' not in f]
+        self.src_list = [f for f in os.listdir(self.src_path) if 'src' in f and '_bk_' not in f and 'bkg' not in f]
+        self.energy = util.json_load(energy)
+        self.energy_split = 50.2 # keV, absorption edges of Gd
+
+    def __get_x_files(self, energy_name:str):
+        return [[os.path.join(self.x_path, f) for f in self.x_ch if energy_name in f and f"_ch{i}_" in f][0] for i in range(4)]
+    def xray_config(self, energy_name:str):
+        read_config = [file_lib.Read_config(ch_file, ending='07', config_file=self.x_config) for ch_file in self.__get_x_files(energy_name)]
+        bkg_read_config = read_config[1:4]
+        bkg_read_config.append(read_config[0])
+        spectrum_config = file_lib.Spectrum_config(corr=self.corr, adc_max=self.adc_max, bin_width=self.bin_width)
+        fit_config = file_lib.Fit_config(self.fit_range[energy_name],self.bkg_form[energy_name])
+        return [read_config, bkg_read_config, spectrum_config, fit_config]
+    def __get_src_bkg(self, name:str):
+        src = name.split('_')[3][:2]
+        return [f for f in os.listdir(self.src_path) if '_bk_' not in f and f'bkg{src}' in f][0]
+    def src_config(self, file):
+        bkg = os.path.join(self.src_path, self.__get_src_bkg(file))
+        file = os.path.join(self.src_path,file)
+        basename = os.path.basename(file)
+        read_config = file_lib.Read_config(file, ending='07')
+        bkg_read_config = file_lib.Read_config(bkg, ending='07')
+        spectrum_config = file_lib.Spectrum_config(corr=self.corr, adc_max=self.adc_max, bin_width=self.bin_width)
+        fit_config = file_lib.Fit_config(self.fit_range[basename],self.bkg_form[basename])
+        return [read_config, bkg_read_config, spectrum_config, fit_config]
+    
 def __get_fp05B(config) -> file_lib.File_operation_05b:
     return file_lib.File_operation_05b(config[0].path, *config)
 def __dict_4ch_reconstruct(dict_4ch):
@@ -284,7 +337,7 @@ def __dict_4ch_reconstruct(dict_4ch):
                 dict_4ch_re[key].append(dict_4ch[i][key][i])
             else:
                 dict_4ch_re[key].append(dict_4ch[i][key])
-        return dict_4ch_re
+    return dict_4ch_re
 def __get_fp03B(config) -> file_lib.File_operation_05b:
     read_config, bkg_read_config, spectrum_config, fit_config = config
     fps = [file_lib.File_operation_05b(read_config[i].path, read_config[i], bkg_read_config[i], spectrum_config, fit_config) for i in range(4)]
