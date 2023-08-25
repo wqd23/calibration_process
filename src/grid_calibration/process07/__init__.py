76236976f833187832c8a610e0db7b050d53717c
刘逸祺 <liuyiqi20@mails.tsinghua.edu.cn>
2023-08-25 21:45:33 +0800

    feature: add ec_operation and json files for 07

--- src/grid_calibration/process07/__init__.py
+++ src/grid_calibration/process07/__init__.py
@@ -1,9 +1,19 @@
-from ..operation import TB_operation_07B
-from .. import util_lib as util
+from ..operation import TB_operation_07B, EC_operation_07B
 tb_op = TB_operation_07B(**{
     "path": "data/07/raw_data/北师大正样温度偏压标定数据-20211124",
     "fit_range": "data/07/single_process/fit_range.json",
     "save_path": "data/07/single_process/TB_fit_result",
     "save_fig_path": "data/07/single_process/single_fit_fig",
     "result_path": "data/07/tb_logs"
+})
+ec_op = EC_operation_07B(**{
+    'tb_result_path' : 'data/07/single_process/temp_bias_fit.json',
+    'fit_range' : "data/07/single_process/fit_range.json",
+    'energy' : 'data/07/single_process/ec_energy.json',
+    'bkg_form' : 'data/07/single_process/bkg_form.json',
+    'x_path' : "data/07/raw_data/X光机标定实验/正样",
+    'src_path' : 'data/07/raw_data/北师大豁免源标定数据-20211127',
+    'save_path' : "data/07/single_process/EC_fit_result",
+    'save_fig_path' : "data/07/single_process/single_fit_fig",
+    'result_path' : "data/07/ec_logs"
 })
