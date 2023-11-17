296900a40c35fe5ec48f3c24237f70f0e85f2843
wqd <wqd.2019@tsinghua.org.cn>
2023-11-17 22:33:43 +0800

    copy reader07 to reader 04

---
+++ lib_reader/src/lib_reader/reader04/read.py
@@ -0,0 +1,8 @@
+from . import gridBasicFunctions02 as basic
+import numpy as np
+from ..reader05.version_lib import data_refactor
+def single_read07(path:str):
+    sciExtracted, telExtracted = basic.dataReadout(path, isHex=True, isBin=False, newProgramme=True)
+    sciExtracted, telExtracted = data_refactor(sciExtracted), data_refactor(telExtracted)
+    
+    return sciExtracted, telExtracted
