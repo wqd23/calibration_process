72a838f6c684c83f958e4592e09098a4ad9c6554
wqd23 <wqd23@mails.tsinghua.edu.cn>
2025-05-13 10:36:34 +0800

    change 11B reader (#9)

--- lib_reader/src/lib_reader/util.py
+++ lib_reader/src/lib_reader/util.py
@@ -3,3 +3,12 @@ def refactor_to4chan(v):
     return [np.array(v[i]) for i in range(4)]
 def data_refactor(sci:dict):
     return {k:v if len(v) != 4 else refactor_to4chan(v) for k,v in sci.items()}
+
+def adict_info(adict):
+    for k,v in adict.items():
+        if isinstance(v, np.ndarray):
+            print(f"{k}:{v.shape},{v.dtype}")
+        if isinstance(v, list) and len(v) == 4:
+            print(f"{k}:{[i.shape for i in v]},{v[0].dtype}")
+        else:
+            print(f"{k}:{v}")
